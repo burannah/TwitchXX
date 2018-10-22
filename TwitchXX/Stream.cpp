b449b86958b4b450453b0122d7aad02d204d1b8f
Andrei Khokhlov <burannah@gmail.com>
2018-10-23 00:30:35 +0100

    Refactor Stream.cpp

--- TwitchXX/Stream.cpp
+++ TwitchXX/Stream.cpp
@@ -10,57 +10,24 @@
 #include <Utility.h>
 #include <Api.h>
 
-std::tuple<std::vector<TwitchXX::Stream>, std::string>
-TwitchXX::getStreams(const Api &api, size_t count, const char *cursor)
+namespace TwitchXX
 {
-    StreamsOptions opt;
-    opt.first = count > 100 || count == 0 ? 20 : count;
-    if(cursor)
+    namespace
     {
-        opt.after = std::string(cursor);
-    }
-
-    opt.type = StreamType::Value::ALL;
-
-    return getStreams(api, opt);
-}
-
-
-std::tuple<std::vector<TwitchXX::Stream>, std::string>
-TwitchXX::getStreams(const Api &api, const StreamsOptions &opt)
-{
-    web::uri_builder builder("helix/streams");
-
-    StreamsOptions::fillBuilder(builder,opt);
-
-    auto response = api.reqWait().get(builder.to_uri());
-    std::vector<Stream> result;
-
-    if (response.has_field("data") && !response.at("data").is_null() && response.at("data").size())
-    {
-        auto data = response.at("data").as_array();
-
-        result.reserve(data.size());
-        std::for_each(data.begin(), data.end(), [&](auto &&val)
+        Stream createStream(const web::json::value& stream)
         {
-            JsonWrapper w(val);
+            JsonWrapper w(stream);
             Stream s;
             s.Id = w["id"];
             s.UserId = w["user_id"];
             s.GameId = w["game_id"];
 
-            //TODO: Support array fields in Property class
-            if(val.has_field("community_ids")
-               && !val.at("community_ids").is_null()
-               && val.at("community_ids").size())
+            if(stream.has_array_field("community_ids"))
             {
-                auto communities = val.at("community_ids").as_array();
-                auto proxy = s.CommunityIds; //I'm too lazy to specify the type
-                std::for_each(communities.begin(), communities.end(), [&](auto&& c)
+                for(const auto& community: stream.at("community_ids").as_array())
                 {
-                    proxy.push_back(c.as_string());
-                });
-                s.CommunityIds = proxy;
+                    s.CommunityIds.push_back(community.as_string());
+                }
             }
 
             s.Type = StreamType::fromString(w["type"].as_string());
@@ -70,19 +37,57 @@ TwitchXX::getStreams(const Api &api, const StreamsOptions &opt)
             s.Language = w["language"].as_string();
             s.Thumb = w["thumbnail_url"].as_string();
 
-            result.push_back(s);
-        });
+            return s;
+        }
     }
 
-    std::string new_cursor;
-    try
+    std::tuple<std::vector<Stream>, std::string>
+    getStreams(const Api &api, size_t count, const char *cursor)
     {
-        new_cursor = response.at("pagination").at("cursor").as_string();
-    }
-    catch(web::json::json_exception& e)
-    {
-        new_cursor = "Error cursor!";
+        StreamsOptions opt;
+        opt.first = count > 100 || count == 0 ? 20 : count;
+        if(cursor)
+        {
+            opt.after = std::string(cursor);
+        }
+
+        opt.type = StreamType::Value::ALL;
+
+        return getStreams(api, opt);
     }
 
-    return std::make_tuple(result, new_cursor);
+
+    std::tuple<std::vector<Stream>, std::string>
+    getStreams(const Api &api, const StreamsOptions &opt)
+    {
+        web::uri_builder builder("helix/streams");
+
+        StreamsOptions::fillBuilder(builder,opt);
+
+        auto response = api.reqWait().get(builder.to_uri());
+        std::vector<Stream> result;
+
+        if (response.has_array_field("data"))
+        {
+            auto data = response.at("data").as_array();
+
+            result.reserve(data.size());
+            for(const auto& stream: data)
+            {
+                result.push_back(createStream(stream));
+            }
+        }
+
+        std::string new_cursor;
+        try
+        {
+            new_cursor = response.at("pagination").at("cursor").as_string();
+        }
+        catch(web::json::json_exception& e)
+        {
+            new_cursor = "Error cursor!";
+        }
+
+        return std::make_tuple(result, new_cursor);
+    }
 }
