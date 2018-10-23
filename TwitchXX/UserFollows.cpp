91a5ad40c33dc03adf38857266afe8532af4ad98
Andrei Khokhlov <burannah@gmail.com>
2018-10-23 23:30:53 +0100

    Refactor UserFollow

--- TwitchXX/UserFollows.cpp
+++ TwitchXX/UserFollows.cpp
@@ -10,57 +10,74 @@
 
 namespace TwitchXX
 {
+    namespace
+    {
+        web::uri getUserFollowsUri(const UserFollowsOptions &options)
+        {
+            if( options.count > 100
+                || (!options.after.empty() && !options.before.empty())
+                || (options.from.empty() && options.to.empty()))
+            {
+                throw TwitchException("GetUserFollows: wrong input parameters");
+            }
+
+            web::uri_builder builder("helix/users/follows");
+            if(!options.after.empty())
+            {
+                builder.append_query("after", options.after);
+            }
+            else if(!options.before.empty())
+            {
+                builder.append_query("before", options.before);
+            }
+
+            builder.append_query("first", options.count);
+
+            if(!options.from.empty())
+            {
+                builder.append_query("from_id", options.from);
+            }
+
+            if(!options.to.empty())
+            {
+                builder.append_query("to_id", options.to);
+            }
+
+            return builder.to_uri();
+        }
+
+        UserFollow createUserFollow(const web::json::value& userFollow)
+        {
+            UserFollow u;
+            JsonWrapper w(userFollow);
+
+            u.from = w["from_id"];
+            u.to = w["to_id"];
+            u.since = DateFromString(w["followed_at"]);
+
+            return u;
+        }
+
+    }
+
     std::tuple<std::vector<UserFollow>, unsigned long long, std::string>
     getUserFollows(const Api &api, const UserFollowsOptions &options)
     {
-        if( options.count > 100
-            || (!options.after.empty() && !options.before.empty())
-            || (options.from.empty() && options.to.empty()))
-        {
-            throw TwitchXX::TwitchException("GetUserFollows: wrong input parameters");
-        }
 
-        web::uri_builder builder("helix/users/follows");
-        if(!options.after.empty())
-        {
-            builder.append_query("after", options.after);
-        }
-        else if(!options.before.empty())
-        {
-            builder.append_query("before", options.before);
-        }
 
-        builder.append_query("first", options.count);
-
-        if(!options.from.empty())
-        {
-            builder.append_query("from_id", options.from);
-        }
-
-        if(!options.to.empty())
-        {
-            builder.append_query("to_id", options.to);
-        }
-
-        auto response = api.reqWait().get(builder.to_uri());
+        auto response = api.reqWait().get(getUserFollowsUri(options));
         auto total = response.at("total").as_number().to_uint64();
         std::vector<UserFollow> result;
 
-        if(response.has_field("data") && !response.at("data").is_null() && response.at("data").size())
+        if(response.has_array_field("data"))
         {
             auto data = response.at("data").as_array();
 
             result.reserve(data.size());
-            std::for_each(std::begin(data), std::end(data), [&](auto&& val)
+            for(const auto& userFollow: data)
             {
-                result.emplace_back();
-                auto& u = result.back();
-                JsonWrapper w(val);
-
-                u.from = w["from_id"];
-                u.to = w["to_id"];
-                u.since = DateFromString(w["followed_at"]);
-            });
+                result.push_back(createUserFollow(userFollow));
+            }
 
         }
 
